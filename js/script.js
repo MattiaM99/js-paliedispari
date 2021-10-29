13893cc515f1c5fc46a5c18e256e95fc84f69f77
MattiaM99 <mattiamazzoni123@gmail.com>
2021-10-29 14:03:23 +0200

    fine parola palindroma

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-let parola = prompt('inserisci parola');
-let parolaDivisa = parola.split('');
-console.log(parolaDivisa);
+let parola = prompt('inserisci una parola')
+
+let parolaInversa = invertiParola(parola);
+
+if(parola == parolaInversa){
+    document.getElementById('parola').innerHTML = 'la parola è palindroma'
+    console.log('la parola è palindroma');
+  } else {
+    document.getElementById('parola').innerHTML = 'la parola non è palindroma'
+    console.log('la parola non è palindroma');
+  }
+  
+function invertiParola(str){
+  let strInversa = str.split('').reverse().join('');  
+  return strInversa;
+}
