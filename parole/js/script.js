930ae167cc43186503ce38e27c93eb2123647d84
MattiaM99 <mattiamazzoni123@gmail.com>
2021-10-29 16:20:58 +0200

    nell'esercizio parole, aggiunta a schermo la parola scelta

--- parole/js/script.js
+++ parole/js/script.js
@@ -1,12 +1,13 @@
 let parola = prompt('inserisci una parola');
 console.log(parola);
-
 let parolaInversa = invertiParola(parola);
 
 if(parola == parolaInversa){
+    document.getElementById('input').innerHTML = 'la tua parola è: ' + parola; 
     document.getElementById('parola').innerHTML = 'la parola è palindroma'
     console.log('la parola è palindroma');
   } else {
+    document.getElementById('input').innerHTML = 'la tua parola è: ' + parola; 
     document.getElementById('parola').innerHTML = 'la parola non è palindroma'
     console.log('la parola non è palindroma');
   };
