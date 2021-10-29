039f379a6d9c0ea3dd96ccfd115f25cbf10a9350
MattiaM99 <mattiamazzoni123@gmail.com>
2021-10-29 18:28:27 +0200

    function fixata, fixare il fatto che chieda prima il numero che pari o dispari

--- pari-dispari/js/script.js
+++ pari-dispari/js/script.js
@@ -4,8 +4,6 @@
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
 
-let scelta = prompt('pari o dispari?');
-document.getElementById('scelta').innerHTML = 'la tua scelta è ' + scelta;
 let pariDispari = parseInt(prompt('inserisci un numero da 1 a 5'));
 document.getElementById('num-utente').innerHTML = 'il tuo numero è: ' + pariDispari;
 console.log(pariDispari);
@@ -18,27 +16,31 @@ function numeroRandom(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min)
 };
 
-const pcRandom = numeroRandom(1, 5);
+let pcRandom = numeroRandom(1, 5);
 document.getElementById('num-comp').innerHTML = 'il numero del computer è: ' + pcRandom;
 console.log(pcRandom);
 
-function somma() {
-  let sommaNumeri = pariDispari + pcRandom;
-  if (sommaNumeri % 2 == 0 && scelta == 'pari') {
+
+function somma(parametroPariDispari, parametroPcRandom) {
+  let parametroScelta = prompt('pari o dispari?');
+  document.getElementById('scelta').innerHTML = 'hai scelto ' + parametroScelta 
+  let sommaNumeri = parametroPariDispari + parametroPcRandom;
+  if (sommaNumeri % 2 == 0 && parametroScelta == 'pari') {
     console.log('il numero è pari');
     document.getElementById('pari-dispari').innerHTML ='la somma tra i due numeri è ' + sommaNumeri + ' ed è pari, hai vinto';
-  }else if (sommaNumeri % 2  && scelta == 'dispari') {
+  }else if (sommaNumeri % 2  && parametroScelta == 'dispari') {
     console.log('il numero è dispari');
     document.getElementById('pari-dispari').innerHTML ='la somma tra i due numeri è ' + sommaNumeri + ' ed è dispari, hai vinto';
-  }else if (sommaNumeri % 2 == 0  && scelta == 'dispari') {
+  }else if (sommaNumeri % 2 == 0  && parametroScelta == 'dispari') {
     console.log('il numero è pari');
     document.getElementById('pari-dispari').innerHTML ='la somma tra i due numeri è ' + sommaNumeri + ' ed è pari, hai perso';
-  }else if (sommaNumeri % 2 && scelta == 'pari') {
+  }else if (sommaNumeri % 2 && parametroScelta == 'pari') {
     console.log('il numero è dispari');
     document.getElementById('pari-dispari').innerHTML ='la somma tra i due numeri è ' + sommaNumeri + ' ed è dispari, hai perso';
   };
   return sommaNumeri;
 };
 
-let risultato = somma();
+
+let risultato = somma(pariDispari, pcRandom);
 console.log(risultato);
